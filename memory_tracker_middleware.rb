1e5439c17ecae2d4e2106eb6f96e0ed85d38670d
hadii <hadivarposhti@gmail.com>
2024-03-02 16:31:13 +0300

    Update memory_tracker_middleware.rb

--- memory_tracker_middleware.rb
+++ memory_tracker_middleware.rb
@@ -10,7 +10,7 @@ class MemoryTrackerMiddleware
   end
 
   def call(env)
-    request_id = SecureRandom.uuid
+    request_id = extract_request_id_from_main_logs(env) || SecureRandom.uuid
     start_time = Time.now
 
     gc_stat_before, memory_before, objects_before, query_cache_before = capture_state
@@ -30,15 +30,29 @@ class MemoryTrackerMiddleware
       objects_after,
       query_cache_before,
       query_cache_after,
-      request_time
+      request_time,
+      status
     )
 
-    log_memory_threshold_exceeded(env, request_id, memory_after - memory_before) if memory_growth_exceeded?(memory_after, memory_before)
+    log_memory_threshold_exceeded(request_id, memory_after - memory_before) if memory_growth_exceeded?(memory_after, memory_before)
 
     [status, headers, body]
   end
 
   private
+
+  def extract_request_id_from_main_logs(env)
+    log_file_path = Rails.root.join('log', 'development.log')
+
+    return unless File.exist?(log_file_path)
+
+    # Read the last line from the development log file
+    last_line = File.readlines(log_file_path).last
+
+    # Extract the request ID from the last line
+    request_id_match = last_line.match(/\[([\w-]+)\]/)
+    request_id_match[1] if request_id_match
+  end
 
   def capture_state
     gc_stats = GC.stat
@@ -55,7 +69,7 @@ class MemoryTrackerMiddleware
     process_info ? process_info.rss * 1024 : 0  # Convert RSS to bytes
   end
 
-  def log_request(env, request_id, gc_stat_before, gc_stat_after, memory_before, memory_after, objects_before, objects_after, query_cache_before, query_cache_after, request_time)
+  def log_request(env, request_id, gc_stat_before, gc_stat_after, memory_before, memory_after, objects_before, objects_after, query_cache_before, query_cache_after, request_time, status)
     log_data = {
       request_id: request_id,
       method: env['REQUEST_METHOD'],
@@ -70,19 +84,20 @@ class MemoryTrackerMiddleware
       objects_after: objects_after,
       query_cache_before: query_cache_before,
       query_cache_after: query_cache_after,
-      request_time: request_time
+      request_time: request_time,
+      status: status
     }
 
     logger.info(log_data.to_json)
   end
 
-  def log_memory_threshold_exceeded(env, request_id, memory_growth)
+  def log_memory_threshold_exceeded(request_id, memory_growth)
     log_data = {
       request_id: request_id,
-      method: env['REQUEST_METHOD'],
-      url: env['REQUEST_URI'],
-      controller: env['action_controller.instance'].class.name,
-      action: env['action_controller.instance'].action_name,
+      method: 'N/A',
+      url: 'N/A',
+      controller: 'N/A',
+      action: 'N/A',
       memory_growth: memory_growth
     }
 
